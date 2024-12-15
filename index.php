7097319a19f380219efdc13caeeaecdbe17921bf
rahmat <rahmat.mondol007@gmail.com>
2024-12-16 04:49:50 +0600

    add logic if subscriptions-for-woocommerce not active

--- index.php
+++ index.php
@@ -25,8 +25,30 @@ function subcription_manager_assets() {
 }
 add_action( 'wp_enqueue_scripts', 'subcription_manager_assets' );
 
-include( __DIR__ . '/inc/shortcodes/subcription_select_box.php' );
-include( __DIR__ . '/inc/functions/get_subcription_info.php' );
-include( __DIR__ . '/inc/functions/wc_account_functions.php' );
-include( __DIR__ . '/inc/functions/check_status.php' );
-include( __DIR__ . '/inc/ajax/save_selected_boxes.php' );
+// Check if Subscriptions for WooCommerce is active
+if ( in_array( 'subscriptions-for-woocommerce/subscriptions-for-woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    // Include necessary files
+    include( __DIR__ . '/inc/acf.php' );
+    include( __DIR__ . '/inc/shortcodes/subcription_select_box.php' );
+    include( __DIR__ . '/inc/functions/get_subcription_info.php' );
+    include( __DIR__ . '/inc/functions/wc_account_functions.php' );
+    include( __DIR__ . '/inc/functions/check_status.php' );
+    include( __DIR__ . '/inc/ajax/save_selected_boxes.php' );
+} else {
+    // Add admin notice to prompt activation of Subscriptions for WooCommerce
+    add_action( 'admin_notices', 'wps_sfw_activation_failure_admin_notice' );
+    add_action( 'network_admin_notices', 'wps_sfw_activation_failure_admin_notice' );
+}
+
+/**
+ * Display an admin notice for WooCommerce Subscriptions activation
+ */
+function wps_sfw_activation_failure_admin_notice() {
+    // Ensure this message is only shown to users with admin privileges
+    if ( current_user_can( 'activate_plugins' ) ) {
+        echo '<div class="notice notice-error is-dismissible">';
+        echo '<p><strong>Subscriptions for WooCommerce is not active.</strong> Please activate it to use this plugin.</p>';
+        echo '</div>';
+    }
+}
+
